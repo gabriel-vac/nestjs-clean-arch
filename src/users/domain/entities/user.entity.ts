ce9559bc089fd97e9bc8a97507dd6c3a7841288d
Gabriel Andrade <gabriel.andrade.guilhen98@gmail.com>
2023-11-13 17:44:19 -0300

    feat/test: getters methods and unit tests

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -9,4 +9,18 @@ export class UserEntity {
   constructor(public readonly props: UserProps) {
     this.props.created_at = this.props.created_at ?? new Date();
   }
+
+  get name() {
+    return this.props.name;
+  }
+  get email() {
+    return this.props.email;
+  }
+  get password() {
+    return this.props.password;
+  }
+
+  get created_at() {
+    return this.props.created_at;
+  }
 }
