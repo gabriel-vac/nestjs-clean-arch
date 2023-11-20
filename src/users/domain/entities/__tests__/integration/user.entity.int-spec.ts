6e3c5f3ad9309b38e0895797db05f87ee30e5c91
Gabriel Andrade <gabriel.andrade.guilhen98@gmail.com>
2023-11-20 20:58:50 -0300

    test: integration test - validate created at field when creating an user

--- src/users/domain/entities/__tests__/integration/user.entity.int-spec.ts
+++ src/users/domain/entities/__tests__/integration/user.entity.int-spec.ts
@@ -79,5 +79,24 @@ describe('UserEntity integration tests', () => {
       };
       expect(() => new UserEntity(props)).toThrow(EntityValidationError);
     });
+
+    it('Should throw an error when creating a user with invalid createdAt', () => {
+      let props: UserProps = {
+        ...UserDataBuilder({}),
+        name: null,
+      };
+
+      props = {
+        ...UserDataBuilder({}),
+        createdAt: '2023' as any,
+      };
+      expect(() => new UserEntity(props)).toThrow(EntityValidationError);
+
+      props = {
+        ...UserDataBuilder({}),
+        createdAt: 10 as any,
+      };
+      expect(() => new UserEntity(props)).toThrow(EntityValidationError);
+    });
   });
 });
