af9c6626e08789a0be2139d5a11aa9a2f52bb757
Gabriel Andrade <gabriel.andrade.guilhen98@gmail.com>
2023-11-20 21:53:02 -0300

    feat: inMemory repository abstract class

--- src/shared/domain/repositories/repository-contracts.ts
+++ src/shared/domain/repositories/repository-contracts.ts
@@ -4,6 +4,6 @@ export interface RepositoryInterface<E extends Entity> {
   insert(entity: E): Promise<void>;
   findById(id: string): Promise<E>;
   findAll(): Promise<E[]>;
-  update(entity: E): Promise<E[]>;
+  update(entity: E): Promise<void>;
   delete(id: string): Promise<void>;
 }
